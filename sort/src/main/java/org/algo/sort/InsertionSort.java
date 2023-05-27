c384d78f6ddac9bf563c09319bd52b7666d42058
Guru Charan Bulusu <gurucb@microsoft.com>
2023-05-27 06:44:19 +0530

    Added more Sort Types

---
+++ sort/src/main/java/org/algo/sort/InsertionSort.java
@@ -0,0 +1,14 @@
+package org.algo.sort;
+
+public class InsertionSort extends Sort{
+
+    @Override
+    public int[] sort(int[] inputElements) {
+        return new int[0];
+    }
+
+    @Override
+    public void sort(String inputFile, String outputFile) {
+
+    }
+}
