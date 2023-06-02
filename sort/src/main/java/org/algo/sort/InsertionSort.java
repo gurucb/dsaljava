30bc4ac006137c5c8ab156d7e676be9ede85a8b1
AshishSingh517 <134363359+AshishSingh517@users.noreply.github.com>
2023-06-02 14:03:07 +0530

    Update InsertionSort.java

--- sort/src/main/java/org/algo/sort/InsertionSort.java
+++ sort/src/main/java/org/algo/sort/InsertionSort.java
@@ -1,14 +1,57 @@
 package org.algo.sort;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
 
-public class InsertionSort extends Sort{
+public class InsertionSort extends Sort {
 
     @Override
     public int[] sort(int[] inputElements) {
-        return new int[0];
+        int n = inputElements.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = inputElements[i];
+            int j = i - 1;
+
+            while (j >= 0 && inputElements[j] > key) {
+                inputElements[j + 1] = inputElements[j];
+                j--;
+            }
+
+            inputElements[j + 1] = key;
+        }
+
+        return inputElements;
     }
 
     @Override
     public void sort(String inputFile, String outputFile) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))) {
 
+            String line = reader.readLine();
+            if (line != null) {
+                String[] numbers = line.split(",");
+                int[] array = new int[numbers.length];
+                for (int i = 0; i < numbers.length; i++) {
+                    array[i] = Integer.parseInt(numbers[i]);
+                }
+
+                int[] sortedArray = sort(array);
+
+                String sortedNumbers = Arrays.toString(sortedArray)
+                        .replace("[", "")
+                        .replace("]", "")
+                        .replace(" ", "");
+
+                writer.write(sortedNumbers);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
+
