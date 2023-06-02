b4069bb6fb5f0324406520d51c933a23c372e293
RADHA SINGH JADAUN <86901025+RADHA16JAN@users.noreply.github.com>
2023-06-02 17:43:13 +0530

    Update QuickSort.java

--- sort/src/main/java/org/algo/sort/QuickSort.java
+++ sort/src/main/java/org/algo/sort/QuickSort.java
@@ -3,11 +3,66 @@ package org.algo.sort;
 public class QuickSort extends Sort{
     @Override
     public int[] sort(int[] inputElements) {
-        return new int[0];
+        int start=0;
+        int end= inputElements.length-1;
+        quick(inputElements,start,end);
+       return inputElements;
+    }
+
+    int partition (int a[], int start, int end) {
+        int pivot = a[end]; // pivot element
+        int i = (start - 1);
+
+        for (int j = start; j <= end - 1; j++) {
+            if (a[j] < pivot) {
+                i++;
+                int t = a[i];
+                a[i] = a[j];
+                a[j] = t;
+            }
+        }
+        int t = a[i+1];
+        a[i+1] = a[end];
+        a[end] = t;
+        return (i + 1);
+    }
+
+
+    void quick(int a[], int start, int end) {
+        if (start < end) {
+            int p = partition(a, start, end);
+            quick(a, start, p - 1);
+            quick(a, p + 1, end);
+        }
     }
 
     @Override
     public void sort(String inputFile, String outputFile) {
+ try {
+            BufferedReader reader=new BufferedReader(new FileReader(inputFile));
+            String line=reader.readLine();
+            reader.close();
 
+            String [] element=line.split(" ,");
+            int [] inputElement=new int[element.length];
+
+            for(int i=0;i<=element.length-1;i++){
+                inputElement[i]=Integer.parseInt(element[i].trim());
+            }
+
+            int [] sortElement=sort(inputElement);
+
+            FileWriter writer=new FileWriter(outputFile);
+            for(int i=0;i<sortElement.length;i++){
+                writer.write(String.valueOf(sortElement[i]));
+                if(i<sortElement.length-1){
+                    writer.write(",");
+                }
+            }
+            writer.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
