34423d0530a8994ad72cfb085891ec5303edf8e8
AshishSingh517 <134363359+AshishSingh517@users.noreply.github.com>
2023-06-02 13:33:34 +0530

    Update Main.java

--- client/src/main/java/org/dei/client/Main.java
+++ client/src/main/java/org/dei/client/Main.java
@@ -1,11 +1,8 @@
 package org.dei.client;
-
-import org.algo.sort.*;
-
-
+import org.algo.sort.SortAdapter;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SortAdapter.sort();
     }
 }
