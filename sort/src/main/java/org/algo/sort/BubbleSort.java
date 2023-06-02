33a746cfd6870465bc21dad2e6b6daf4409ad342
RADHA SINGH JADAUN <86901025+RADHA16JAN@users.noreply.github.com>
2023-06-02 09:01:44 +0530

    Update BubbleSort.java

--- sort/src/main/java/org/algo/sort/BubbleSort.java
+++ sort/src/main/java/org/algo/sort/BubbleSort.java
@@ -3,11 +3,47 @@ package org.algo.sort;
 public class BubbleSort extends Sort{
     @Override
     public int[] sort(int[] inputElements) {
-        return new int[0];
+      int n=inputElements.length;
+        for(int i=0;i<n;i++){
+            for(int j=1;j<n-i-1;j++){
+                if(inputElements[j]>inputElements[j+1]){
+                    int temp=inputElements[j];
+                    inputElements[j]=inputElements[j+1];
+                    inputElements[j+1]=temp;
+                }
+            }
+        }
+
+        return inputElements;
     }
 
     @Override
     public void sort(String inputFile, String outputFile) {
+ try {
+            BufferedReader reader=new BufferedReader(new FileReader(inputFile));
+            String line=reader.readLine();
+            reader.close();
 
+            String [] element=line.split(" ,");
+            int [] inputElement=new int[element.length];
+
+            for(int i=0;i<=element.length-1;i++){
+                inputElement[i]=Integer.parseInt(element[i].trim());
+            }
+
+            int [] sortElement=sort(inputElement);
+
+            FileWriter writer=new FileWriter(outputFile);
+            for(int i=0;i<sortElement.length;i++){
+                writer.write(String.valueOf(sortElement[i]));
+                if(i<sortElement.length-1){
+                    writer.write(",");
+                }
+            }
+            writer.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
