a79c6638f461cc75e499e3316d7685ed7a1feab2
AshishSingh517 <134363359+AshishSingh517@users.noreply.github.com>
2023-06-02 14:03:54 +0530

    Update SelectionSort.java

--- sort/src/main/java/org/algo/sort/SelectionSort.java
+++ sort/src/main/java/org/algo/sort/SelectionSort.java
@@ -1,13 +1,56 @@
 package org.algo.sort;
 
-public class SelectionSort extends Sort{
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class SelectionSort extends Sort {
+
     @Override
     public int[] sort(int[] inputElements) {
-        return new int[0];
+        int n = inputElements.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (inputElements[j] < inputElements[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = inputElements[minIndex];
+            inputElements[minIndex] = inputElements[i];
+            inputElements[i] = temp;
+        }
+        return inputElements;
     }
 
     @Override
     public void sort(String inputFile, String outputFile) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))) {
 
+            String line = reader.readLine();
+            if (line != null) {
+                String[] numbers = line.split(",");
+                int[] array = new int[numbers.length];
+                for (int i = 0; i < numbers.length; i++) {
+                    array[i] = Integer.parseInt(numbers[i]);
+                }
+
+                int[] sortedArray = sort(array);
+
+                String sortedNumbers = Arrays.toString(sortedArray)
+                        .replace("[", "")
+                        .replace("]", "")
+                        .replace(" ", "");
+
+                writer.write(sortedNumbers);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
