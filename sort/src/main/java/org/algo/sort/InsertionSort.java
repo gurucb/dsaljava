58a97f525ca4ff55bdd5ce189d15955d3d814c2e
RADHA SINGH JADAUN <86901025+RADHA16JAN@users.noreply.github.com>
2023-06-02 14:54:55 +0530

    Update InsertionSort.java

--- sort/src/main/java/org/algo/sort/InsertionSort.java
+++ sort/src/main/java/org/algo/sort/InsertionSort.java
@@ -4,11 +4,47 @@ public class InsertionSort extends Sort{
 
     @Override
     public int[] sort(int[] inputElements) {
-        return new int[0];
+         int n = inputElements.length;
+        for (int j = 1; j < n; j++) {
+            int key = inputElements[j];
+            int i = j-1;
+            while((i > -1) && ( inputElements [i] > key)) {
+                inputElements [i+1] = inputElements [i];
+                i--;
+            }
+            inputElements[i+1] = key;
+        }
+
+        return inputElements;
     }
 
     @Override
     public void sort(String inputFile, String outputFile) {
+ try {
+            BufferedReader reader=new BufferedReader(new FileReader(inputFile));
+            String line=reader.readLine();
+            reader.close();
 
+            String [] element=line.split(" ,");
+            int [] inputElement=new int[element.length];
+
+            for(int i=0;i<=element.length-1;i++){
+                inputElement[i]=Integer.parseInt(element[i].trim());
+            }
+
+            int [] sortElement=sort(inputElement);
+
+            FileWriter writer=new FileWriter(outputFile);
+            for(int i=0;i<sortElement.length;i++){
+                writer.write(String.valueOf(sortElement[i]));
+                if(i<sortElement.length-1){
+                    writer.write(",");
+                }
+            }
+            writer.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
