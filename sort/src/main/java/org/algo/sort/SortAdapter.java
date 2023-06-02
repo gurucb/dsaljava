9907a6b8919732a14a2a2f0c1c5c86272cc59a9e
RADHA SINGH JADAUN <86901025+RADHA16JAN@users.noreply.github.com>
2023-06-02 08:50:26 +0530

    Update SortAdapter.java

--- sort/src/main/java/org/algo/sort/SortAdapter.java
+++ sort/src/main/java/org/algo/sort/SortAdapter.java
@@ -3,7 +3,40 @@ package org.algo.sort;
 public class SortAdapter {
     public static Sort getSortObject(String sortType)
     {
-        Sort sort = new MergeSort();
-        return sort;
+        if(sortType.equals("BubbleSort")){
+           return new BubbleSort();
+       }
+
+        if(sortType.equals("BucketSort")){
+            return new BucketSort();
+        }
+
+        if(sortType.equals("HeapSort")){
+            return new HeapSort();
+        }
+
+        if(sortType.equals("InsertionSort")){
+            return new InsertionSort();
+        }
+
+        if(sortType.equals("MergeSort")){
+            return new MergeSort();
+        }
+
+        if(sortType.equals("QuickSort")){
+            return new QuickSort();
+        }
+
+        if(sortType.equals("SelectionSort")){
+            return new SelectionSort();
+        }
+
+        if(sortType.equals("ShellSort")){
+            return new ShellSort();
+        }
+
+
+        return new MergeSort();
+    
     }
 }
